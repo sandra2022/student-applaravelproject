5972e15c2055131226599ba8eeaa032d9978a1bb
sandra2022 <mca544@rajagiri.edu>
2021-03-17 11:59:53 +0530

    studentl

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<table class="table">
+        
+      <div>
+        <div class="col col-sm-6 col-lg-6 col-xl-6 col-col-6">
+
+          <tr>
+          <td>Name</td>
+            <td><input class="form-control" type="text"></td>
+          </tr>
+
+          <tr>
+          <td>Rollno</td>
+            <td><input class="form-control" type="text"></td>
+          </tr>
+
+          <tr>
+          <td>Admission</td>
+            <td><input class="form-control" type="text"></td>
+            </tr>
+
+            <tr>
+            <td>College</td>
+            <td><input class="form-control" type="text"></td>
+            </tr>
+
+            <tr>
+                <td></td>
+            <td><button class="btn btn-success">SUBMIT</button></td>
+        </tr>
+</table>
+</body>
+</html>
+
+
+
